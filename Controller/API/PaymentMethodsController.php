87d9b6a6b186aa714d81135855c31d05f9c82011
Kareem Shendy <kareem.elshendy@ibtikar.net.sa>
2016-10-04 17:27:52 +0200

    edit payment method

--- Controller/API/PaymentMethodsController.php
+++ Controller/API/PaymentMethodsController.php
@@ -24,7 +24,14 @@ class PaymentMethodsController extends Controller
      *      {"name"="merchantReference", "dataType"="string", "required"=true},
      *      {"name"="fortId", "dataType"="string", "required"=true}
      *  },
-     *  output="Ibtikar\ShareEconomyToolsBundle\APIResponse\Success"
+     *  statusCodes = {
+     *      200="Returned on success",
+     *      422="Returned if there is a validation error in the sent data",
+     *  },
+     *  responseMap = {
+     *      200="Ibtikar\ShareEconomyToolsBundle\APIResponse\Success",
+     *      422="Ibtikar\ShareEconomyToolsBundle\APIResponse\ValidationErrors",
+     *  }
      * )
      * @author Karim Shendy <kareem.elshendy@ibtikar.net.sa>
      * @return JsonResponse
@@ -68,6 +75,80 @@ class PaymentMethodsController extends Controller
     }
 
     /**
+     * edit payment method
+     *
+     * @ApiDoc(
+     *  section="PayFort",
+     *  parameters={
+     *      {"name"="cardNumber", "dataType"="string", "required"=true},
+     *      {"name"="expiryDate", "dataType"="string", "required"=true},
+     *      {"name"="tokenName", "dataType"="string", "required"=true},
+     *      {"name"="paymentOption", "dataType"="string", "required"=false},
+     *      {"name"="merchantReference", "dataType"="string", "required"=true},
+     *      {"name"="fortId", "dataType"="string", "required"=true}
+     *  },
+     *  statusCodes = {
+     *      200="Returned on success",
+     *      422="Returned if there is a validation error in the sent data",
+     *  },
+     *  responseMap = {
+     *      200="Ibtikar\ShareEconomyToolsBundle\APIResponse\Success",
+     *      422="Ibtikar\ShareEconomyToolsBundle\APIResponse\ValidationErrors",
+     *  }
+     * )
+     * @author Karim Shendy <kareem.elshendy@ibtikar.net.sa>
+     * @return JsonResponse
+     */
+    public function editPaymentMethodAction(Request $request, $id)
+    {
+        $em            = $this->getDoctrine()->getManager();
+        $user          = $this->getUser();
+        $paymentMethod = $em->getRepository('IbtikarShareEconomyPayFortBundle:PfPaymentMethod')->find($id);
+
+        if (!$paymentMethod) {
+            return new JsonResponse(new ToolsBundleAPIResponses\NotFound());
+        } elseif ($paymentMethod->getHolder()->getId() !== $user->getId()) {
+            $output = new JsonResponse(new ToolsBundleAPIResponses\AccessDenied());
+        } else {
+            $paymentMethod->setTokenName(null);
+
+            $newPaymentMethod = new \Ibtikar\ShareEconomyPayFortBundle\Entity\PfPaymentMethod();
+            $newPaymentMethod->setHolder($user);
+            $newPaymentMethod->setCardNumber($request->request->get('cardNumber'));
+            $newPaymentMethod->setExpiryDate($request->request->get('expiryDate'));
+            $newPaymentMethod->setTokenName($request->request->get('tokenName'));
+            $newPaymentMethod->setPaymentOption($request->request->get('paymentOption'));
+            $newPaymentMethod->setMerchantReference($request->request->get('merchantReference'));
+            $newPaymentMethod->setFortId($request->request->get('fortId'));
+            $newPaymentMethod->setIsDefault($paymentMethod->getIsDefault());
+
+            $validationMessages = $this->get('api_operations')->validateObject($newPaymentMethod);
+
+            if (count($validationMessages)) {
+                $output         = new ToolsBundleAPIResponses\ValidationErrors();
+                $output->errors = $validationMessages;
+            } else {
+                $em->persist($newPaymentMethod);
+
+                try {
+                    $em->flush();
+
+                    $em->remove($paymentMethod);
+                    $em->flush();
+
+                    $output = new ToolsBundleAPIResponses\Success();
+                } catch (\Exception $exc) {
+                    $output = new ToolsBundleAPIResponses\InternalServerError();
+
+                    $this->get('logger')->error($exc->getMessage());
+                }
+            }
+        }
+
+        return new JsonResponse($output);
+    }
+
+    /**
      * set payment method as default
      *
      * @ApiDoc(
@@ -84,13 +165,13 @@ class PaymentMethodsController extends Controller
         $paymentMethod = $em->getRepository('IbtikarShareEconomyPayFortBundle:PfPaymentMethod')->find($id);
 
         if (!$paymentMethod) {
-            return new JsonResponse(new ToolsBundleAPIResponses\EntityNotFoundResponse());
+            $output = new JsonResponse(new ToolsBundleAPIResponses\NotFound());
         } elseif ($paymentMethod->getHolder()->getId() !== $user->getId()) {
             $output = new JsonResponse(new ToolsBundleAPIResponses\AccessDenied());
         } else {
             $defaultPaymentMethod = $em->getRepository('IbtikarShareEconomyPayFortBundle:PfPaymentMethod')->findOneBy(['holder' => $user, 'isDefault' => true]);
 
-            if ($defaultPaymentMethod){
+            if ($defaultPaymentMethod) {
                 $defaultPaymentMethod->setIsDefault(false);
             }
 
