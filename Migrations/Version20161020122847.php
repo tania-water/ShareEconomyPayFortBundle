f6815a44be822ca21294df05ab44cb35fbdf9397
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-20 12:37:40 +0200

    Enhancement: Add migration file for the new trait columns

---
+++ Migrations/Version20161020122847.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibtikar\ShareEconomyPayFortBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161020122847 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pf_payment_method ADD updated_at DATETIME NOT NULL, ADD version INT DEFAULT 1 NOT NULL, CHANGE created_at created_at DATETIME NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pf_payment_method DROP updated_at, DROP version, CHANGE created_at created_at DATETIME DEFAULT NULL');
+    }
+}
